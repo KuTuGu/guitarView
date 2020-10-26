8638237f556c60f07f37963f07b8bcd8950bf198
kutugu <zhongliwang48@gmail.com>
2020-10-26 21:55:21 +0800

    feat: add FLIP animation

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+/**
+ * FLIP动画函数，平滑动画，提升性能
+ * @function FLIPAnimation
+ * @param    { HTMLElement }  el             执行动画的具体dom节点
+ * @param    { String }       lastClass      节点最终位置的类选择器
+ * 
+ * @return   { Function }     animate        动画执行函数
+ */
+function FLIPAnimation(el, lastClass) {
+  // 获取节点初始样式
+  const firstStyle = el.getBoundingClientRect();
+  const firstOpacity = parseFloat(getComputedStyle(el).opacity);
+
+  // 为节点指定样式，使其定位到最终位置
+  el.classList.add(lastClass);
+  // 获取节点最终位置
+  const lastStyle = el.getBoundingClientRect();
+  const lastOpacity = parseFloat(getComputedStyle(el).opacity);
+
+  // 反转，恢复到初始样式
+  const invert = {
+    left: firstStyle.left - lastStyle.left,
+    top: firstStyle.top - lastStyle.top,
+    width: firstStyle.width / lastStyle.width,
+    height: firstStyle.height / lastStyle.height
+  };
+  const invertTran = `
+    translate(${invert.left}px, ${invert.top}px)
+    scale(${invert.width}, ${invert.height})
+  `;
+  el.style.transformOrigin = '0 0';
+  el.style.transform = invertTran;
+  el.style.opacity = firstOpacity;
+
+  // 返回动画函数
+  let isReverse = false;
+  return transition => {
+    el.style.transition = transition || el.style.transition;
+
+    if (isReverse) {
+      isReverse = !isReverse;
+      requestAnimationFrame(() => {
+        el.style.transform = invertTran;
+        el.style.opacity = firstOpacity;
+      })
+    } else {
+      isReverse = !isReverse;
+      requestAnimationFrame(() => {
+        el.style.opacity = lastOpacity;
+        el.style.transform = `translate(0, 0) scale(1, 1)`;
+      })
+    }
+  }
+}
+
 /*
 1.对goods监听click冒泡，直接处理good对应的dom节点
 2.对全局监听click冒泡，需要在e.target绑定good信息，即good下所有可点击dom子节点都需要绑定good信息
@@ -6,74 +61,43 @@
 */
 window.onload = () => {
   let isExpanded = false;
-  // 存储dom指针，避免频繁读取
-  let goodRef = null;
+  let reverseArr = null;
   const goods = document.getElementsByClassName('good');
-  const leftView = document.getElementById('leftView');
   const switchOff = document.getElementsByClassName('switch');
   const topMask = document.getElementsByClassName('topMask')[0];
+  const transition = 'transform 0.5s ease-in-out';
 
   Array.from(goods).forEach(dom => {
+    const [ img, mask ] = dom.children[0].children;
+    const [ title, desc, detail ] = dom.children[1].children;
+    const animations = [
+      // 注意可能存在顺序前后影响
+      FLIPAnimation(desc, 'descExpanded'),
+      FLIPAnimation(detail, 'detailExpanded'),
+      FLIPAnimation(title, 'titleExpanded'),
+      FLIPAnimation(mask, 'maskExpanded'),
+      FLIPAnimation(img, 'goodPicExpanded'),
+      FLIPAnimation(topMask, 'topMaskExpanded'),
+      FLIPAnimation(switchOff[0], 'hide'),
+      FLIPAnimation(switchOff[1], 'show')
+    ];
+
     dom.addEventListener('click', () => {
       if (!isExpanded) {
-        requestAnimationFrame(() => {
-          isExpanded = true;
-          const [ img, mask ] = dom.children[0].children;
-          const [ title, desc, detail ] = dom.children[1].children;
-          // 获取相对位置，遮罩进行相应偏移
-          const { bottom } = mask.getBoundingClientRect();
-          const left = leftView.contains(dom);
-          
-          img.classList.add('goodPicExpanded');
-          // 偏移值需要js计算
-          img.style.transform = `translate(
-            ${left ? "45vw" : "-5vw"},
-            ${window.innerHeight * 0.8 - bottom}px
-          )`;
+        isExpanded = true;
+        
+        animations.forEach(animate => animate(transition));
 
-          topMask.classList.add('maskExpanded');
-          mask.classList.add('maskExpanded');
-          mask.style.transform = `translate(
-            ${left ? "-5vw" : "-55vw"},
-            ${window.innerHeight - bottom}px
-          )`;
-
-          switchOff[0].style.opacity = 0;
-          switchOff[1].style.opacity = 1;
-
-          title.classList.add("title-expanded");
-          desc.classList.add("desc-expanded");
-          detail.classList.add("detail-expanded");
-          
-          // 存储
-          goodRef = dom;
-        })
+        reverseArr = animations;
       }
     })
   })
 
   switchOff[1].addEventListener('click', () => {
     if (isExpanded) {
-      requestAnimationFrame(() => {
-        isExpanded = false;
-        // 恢复原布局
-        const [ img, mask ] = goodRef.children[0].children;
-        const [ title, desc, detail ] = goodRef.children[1].children;
-
-        title.classList.remove("title-expanded");
-        desc.classList.remove("desc-expanded");
-        detail.classList.remove("detail-expanded");
-        
-        img.classList.remove('goodPicExpanded');
-        img.style.transform = 'translate(0, 0)';
-  
-        topMask.classList.remove('maskExpanded');
-        mask.classList.remove('maskExpanded');
-        mask.style.transform = 'translate(0, 0)';
-  
-        switchOff[0].style.opacity = 1;
-        switchOff[1].style.opacity = 0;
-      })
+      isExpanded = false;
+      
+      reverseArr.forEach(clearAnimation => clearAnimation(transition));
     }
   })
 }
